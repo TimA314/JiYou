31531ac000db6f34cf6824631565f85bc6e57988
TimA314 <ci0d7j9m@mailer.me>
2023-03-22 20:15:42 -0700

    remove uneeded files

--- jiyou/src/nostr/Event.ts
+++ jiyou/src/nostr/Event.ts
@@ -1,9 +1,55 @@
 export type Event = {
-    id: string,
-    kind: number,
-    pubkey: string,
-    sig: string,
-    tags: object,
-    created_at: number,
+    id?: string
+    sig?: string
+    kind: Kind
+    tags: string[][]
+    pubkey: string
     content: string
-}
+    created_at: number
+  }
+
+export enum Kind {
+    Metadata = 0,
+    Text = 1,
+    RecommendRelay = 2,
+    Contacts = 3,
+    EncryptedDirectMessage = 4,
+    EventDeletion = 5,
+    EventUpdate = 6,
+    Reaction = 7,
+    ChannelCreation = 40,
+    ChannelMetadata = 41,
+    ChannelMessage = 42,
+    ChannelHideMessage = 43,
+    ChannelMuteUser = 44,
+    RelayList = 10002,
+    BlockList = 16462,
+    FlagList = 16463,
+    ReplaceableByTag = 30000,
+  }
+
+  export function getNewEvent(): Event {
+    return {
+      kind: 0,
+      pubkey: '',
+      content: '',
+      tags: [],
+      created_at: 0
+    }
+  }
+
+  export function isEventValid(event: Event): boolean {
+    if (typeof event.content !== 'string') return false
+    if (typeof event.created_at !== 'number') return false
+    if (typeof event.pubkey !== 'string') return false
+    if (!event.pubkey.match(/^[a-f0-9]{64}$/)) return false
+    if (!Array.isArray(event.tags)) return false
+    for (let i = 0; i < event.tags.length; i++) {
+      let tag = event.tags[i]
+      if (!Array.isArray(tag)) return false
+      for (let j = 0; j < tag.length; j++) {
+        if (typeof tag[j] === 'object') return false
+      }
+    }
+    return true
+  }
---
+++ jiyou/src/pages/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const SignIn = (props: Props) => {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default SignIn
