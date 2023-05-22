77919a310b3c55c93dd6804e7e0a59e7adab2696
Tim Amburn <91898542+TimA314@users.noreply.github.com>
2023-05-21 18:32:27 -0700

    nostr extension required to view followers feed

--- src/pages/GlobalFeed.tsx
+++ src/pages/GlobalFeed.tsx
@@ -29,6 +29,10 @@ type GlobalFeedProps = {
 
     //global or followers
     const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+        if (!window.nostr){
+            alert("Install a Nostr Extension to see your followers feed")
+            return;
+        }
         setTabIndex(newValue);
         setEvents([]);
     };
