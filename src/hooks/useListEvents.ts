29a501e81d6eb5754441ee1c8bc5d02e6b9de7ed
Tim Amburn <timothyamburn@dev1.mailer.me>
2023-06-27 20:06:53 -0500

    Quick improvements (#33)
    * add metadata helping relay
    
    * move code and remove console.log

--- src/hooks/useListEvents.ts
+++ src/hooks/useListEvents.ts
@@ -4,6 +4,7 @@ import { sanitizeEvent } from '../utils/sanitizeUtils';
 import { FullEventData, MetaData, ReactionCounts, RelaySetting } from '../nostr/Types';
 import { getEventOptions } from '../nostr/FeedEvents';
 import { eventContainsExplicitContent, setEventData } from '../utils/eventUtils';
+import { metaDataAndRelayHelpingRelay } from '../utils/miscUtils';
 
 type useListEventsProps = {
   pool: SimplePool | null;
@@ -95,7 +96,7 @@ export const useListEvents = ({
           });
         });
         
-        const fetchedMetaDataEvents = await pool.list(allRelayUrls, [{kinds: [0], authors: eventsPubkeys}]);
+        const fetchedMetaDataEvents = await pool.list([...new Set([...allRelayUrls, metaDataAndRelayHelpingRelay])], [{kinds: [0], authors: eventsPubkeys}]);
         
         const metaDataMap: Record<string, MetaData> = {};
         fetchedMetaDataEvents.forEach((event: Event) => {
