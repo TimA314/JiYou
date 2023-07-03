afd101b6439f829411a354e306760262d1601527
Tim Amburn <91898542+TimA314@users.noreply.github.com>
2023-07-03 16:19:32 -0700

    make responsive for mobile screens

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -1,20 +1,33 @@
 import { Stack } from "@mui/material";
 import { Event } from "nostr-tools";
 import UserNotificationNote from "./UserNotificationNote";
-import { MetaData } from "../nostr/Types";
+import { FullEventData, MetaData } from "../nostr/Types";
 
 type Props = {
   likedNotificationEvents: Event[];
   likedNotificationMetaData: Record<string, MetaData>;
+  userNotes: FullEventData[];
 }
 
-export default function Notifications({likedNotificationEvents, likedNotificationMetaData}: Props) {
+export default function Notifications({likedNotificationEvents, likedNotificationMetaData, userNotes}: Props) {
   const uniqueEvents = [...new Set(likedNotificationEvents)];
+
   return (
     <Stack>
       {uniqueEvents.map((event) => {
+        const likedNoteEventId = event.tags.find((tag) => tag[0] === "e") || "";
+        
+        const likedNote = userNotes.find((note) => note.eventId === likedNoteEventId[1])
+
+        if (!likedNote) return (<></>)
+
         return (
-          <UserNotificationNote key={event.sig} event={event} metaData={likedNotificationMetaData[event.pubkey]} />
+          <UserNotificationNote 
+            key={event.sig} 
+            event={event} 
+            metaData={likedNotificationMetaData[event.pubkey]} 
+            userNote={likedNote}
+            />
         )
       })}
     </Stack>
