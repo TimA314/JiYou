3c45ae923ef5a0e86c13cee99b25fd35bdc4bf59
Tim Amburn <timothyamburn@dev1.mailer.me>
2023-07-06 21:07:48 -0500

    chunk events for efficiency (#47)

--- src/hooks/useListEvents.ts
+++ src/hooks/useListEvents.ts
@@ -45,55 +45,74 @@ export const useListEvents = ({
   const filter = useRef<Filter | null>(null);
   const readableRelayUrls = useMemo(() => relays.filter((r) => r.read).map((r) => r.relayUrl), [relays]);
   const allRelayUrls = useMemo(() => relays.map((r) => r.relayUrl), [relays]);
-
-  const fetchThreadEvents = async (eventDataSet: FullEventData[]) => {
-    if (!pool) return;
-
+  const CHUNK_SIZE = 15;
+  
+  const fetchThreadEvents = async (eventDataSet: FullEventData[], startIndex = 0) => {
+    if (!pool) {
+      return {
+        fetchedReplyEvents: [],
+        fetchedRootEvents: [],
+        chunk: [],
+      };
+    }
+  
+    const chunk = eventDataSet.slice(startIndex, startIndex + CHUNK_SIZE);
+  
     //Reply Events
-    const replyFilter = { "kinds": [1], "#e": eventDataSet.map((e) => e.eventId)};
-
+    const replyFilter = { "kinds": [1], "#e": chunk.map((e) => e.eventId)};
     const fetchedReplyEvents = await fetchNostrEvent(pool, allRelayUrls, allRelayUrls, replyFilter, hideExplicitContent.current)
-    let newReplyEvents = new Map(threadEvents.replyEvents);
-    fetchedReplyEvents.forEach((event) => {
-      newReplyEvents.set(event.eventId, event);
-    });
-
+  
     //Root Events
     const rootEventIdsToFetch: string[] = [];
     const relaysToFetchFrom: string[] = [];
-
-    eventDataSet.forEach((f) => {
+  
+    chunk.forEach((f) => {
       const eventIdsFromTags = f.tags.filter((t) => t[0] === "e" && t[1] && t[1] !== f.eventId).map((t) => t[1]);
       const recommendedEventRelays = f.tags.filter((t) => t[2] && t[2].startsWith("wss")).map((t) => t[2]);
-
-        eventIdsFromTags.forEach((e) => {
-          if (threadEvents.rootEvents.has(e)) return;
-          rootEventIdsToFetch.push(e);
-        });
-        relaysToFetchFrom.push(...recommendedEventRelays);
+  
+      eventIdsFromTags.forEach((e) => {
+        if (threadEvents.rootEvents.has(e)) return;
+        rootEventIdsToFetch.push(e);
+      });
+      relaysToFetchFrom.push(...recommendedEventRelays);
     });
-
+  
     const rootFilter = { "kinds": [1], ids: rootEventIdsToFetch};
-
     const fetchedRootEvents = await fetchNostrEvent(pool, [...new Set([...allRelayUrls, ...relaysToFetchFrom])], allRelayUrls, rootFilter, hideExplicitContent.current)
-    let newRootEvents = new Map(threadEvents.rootEvents);
-    fetchedRootEvents.forEach((event) => {
-      newRootEvents.set(event.eventId, event);
+  
+    const threadEventResult = {
+      fetchedReplyEvents,
+      fetchedRootEvents,
+      chunk
+    };
+  
+    setThreadEvents(prevState => {      
+      let newReplyEvents = new Map(prevState.replyEvents);
+      threadEventResult.fetchedReplyEvents.forEach((event) => {
+        newReplyEvents.set(event.eventId, event);
+      });
+  
+      let newRootEvents = new Map(prevState.rootEvents);
+      threadEventResult.fetchedRootEvents.forEach((event) => {
+        newRootEvents.set(event.eventId, event);
+      });
+  
+      const feedEventsMap = new Map(threadEventResult.chunk.map(event => [event.eventId, event]));
+  
+      //if images only mode, filter out events with no images
+      const filteredFeedEventsMap = imagesOnlyMode.current ? 
+          new Map([...feedEventsMap].filter(([,value]) => value.images.length > 0)) : 
+          feedEventsMap;
+  
+      return {
+        feedEvents: new Map([...prevState.feedEvents, ...filteredFeedEventsMap]),
+        replyEvents: newReplyEvents, 
+        rootEvents: newRootEvents
+      }
     });
-
-
-    if (imagesOnlyMode.current) {
-      setThreadEvents({
-        feedEvents: new Map(eventDataSet.filter((e) => e.images.length > 0).map(event => [event.eventId, event])), 
-        replyEvents: newReplyEvents, 
-        rootEvents: newRootEvents
-      });
-    } else {
-      setThreadEvents({
-        feedEvents: new Map(eventDataSet.map(event => [event.eventId, event])),
-        replyEvents: newReplyEvents, 
-        rootEvents: newRootEvents
-      });
+  
+    if (startIndex + CHUNK_SIZE < eventDataSet.length) {
+      fetchThreadEvents(eventDataSet, startIndex + CHUNK_SIZE);
     }
   }
 
@@ -101,29 +120,29 @@ export const useListEvents = ({
     try {
       if (!pool) return;
       fetchingEventsInProgress.current = true;
-
+  
       //Reset events
       setThreadEvents({
         feedEvents: new Map(),
         replyEvents: new Map(), 
         rootEvents: new Map()
       });
-
+  
       //If no followers and on the followers tab, don't fetch events
       if (tabIndex === 1 && following.length === 0) {
         setFetchEvents(false);
         fetchingEventsInProgress.current = false;
         return;
       }
-
+  
       const filterToUse = filter.current ? filter.current : getDefaultFeedFilter(hashtags, tabIndex, following);
       
       console.log('Fetching events with options: ', filterToUse );
-
+  
       const eventDataSet = await fetchNostrEvent(pool, readableRelayUrls, allRelayUrls, filterToUse, hideExplicitContent.current)
       
       await fetchThreadEvents(eventDataSet);
-
+  
     } catch (error) {
       console.error('Error fetching events:', error);
     } finally {
