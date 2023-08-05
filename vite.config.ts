8d259f620845bec7ac054670da20477d39cd65a4
Tim Amburn <timothyamburn@dev1.mailer.me>
2023-08-05 15:03:03 -0500

    Vite pwa updates (#59)
    * install vite pwa plugin
    
    * updates to vite PWA logic
    
    * git ignore package-lock.json
    
    * remove old manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,43 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(), 
+    viteTsconfigPaths(), 
+    svgrPlugin(), 
+    VitePWA({
+      filename: 'sw.ts',
+      includeAssets: ['*.svg'],
+      manifest: {
+        "name": "JiYou",
+        "short_name": "JiYou",
+        "theme_color": "#ffffff",
+        "background_color": "#ffffff",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "orientation": "portrait-primary",
+        "icons": [
+          {
+            "src": "/public/JiYouIcon.svg",
+            "sizes": "192x192",
+            "type": "image/svg"
+          },
+          {
+            "src": "/public/JiYouIcon.svg",
+            "sizes": "512x512",
+            "type": "image/svg"
+          }
+        ]
+      },
+      srcDir: 'src/service-worker',
+      strategies: 'injectManifest'
+    })
+  ],
   resolve: {
     alias: {
       '@okta/okta-auth-js': '@okta/okta-auth-js/dist/okta-auth-js.umd.js',
