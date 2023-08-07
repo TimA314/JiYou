f1e9e2c094cc1404e50254a6a315b0e56fe1bcba
Tim Amburn <91898542+TimA314@users.noreply.github.com>
2023-08-06 17:55:23 -0700

    add default relay

--- src/nostr/DefaultRelays.ts
+++ src/nostr/DefaultRelays.ts
@@ -3,6 +3,8 @@ import { RelaySetting } from "./Types";
 export const defaultRelays: RelaySetting[]  = [
     {relayUrl: "wss://nostr.wine", read: true, write: false},
     {relayUrl: "wss://eden.nostr.land", read: true, write: false},
+    {relayUrl: "wss://relay.nostr.band/", read: true, write: false},
+    {relayUrl: "wss://relay.nostr.band/all", read: false, write: true},
     {relayUrl: "wss://relay.snort.social", read: false, write: true},
     {relayUrl: "wss://nostr.bitcoiner.social", read: false, write: true},
     {relayUrl: "wss://relay.nostrgraph.net", read: false, write: true},
@@ -10,5 +12,4 @@ export const defaultRelays: RelaySetting[]  = [
     {relayUrl: "wss://relay.nostr.bg/", read: false, write: true},
     {relayUrl: "wss://nostr.fmt.wiz.biz/", read: false, write: true},
     {relayUrl: "wss://nos.lol/", read: false, write: true},
-    {relayUrl: "wss://relay.nostr.band/", read: false, write: true},
   ]
